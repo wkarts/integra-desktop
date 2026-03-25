e2fe4b1caf8a6d09470a195e449750166164e935
wkarts <57051272+wkarts@users.noreply.github.com>
2026-03-24 23:48:10 -0300

    fix: alinhar integração de licenciamento no fluxo Tauri

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider } from 'react-router-dom';
 import { AppRouter } from './router';
 import { NfseStoreProvider } from '../modules/nfse-servicos/stores/NfseStore';
 import { StartupRegistrationGate } from '../modules/licensing/components/StartupRegistrationGate';
+import { LicenseRuntimeProvider } from '../modules/licensing/context/LicenseRuntimeContext';
 
 export default function App() {
   return (
     <NfseStoreProvider>
-      <StartupRegistrationGate />
-      <RouterProvider router={AppRouter} />
+      <LicenseRuntimeProvider>
+        <StartupRegistrationGate />
+        <RouterProvider router={AppRouter} />
+      </LicenseRuntimeProvider>
     </NfseStoreProvider>
   );
 }
